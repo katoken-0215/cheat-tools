fec9f8b23f6e7efbf3f4cfa2b010714b155656b6
katoken-0215 <katoken@katoken-0215.sakura.ne.jp>
2014-01-09 23:27:21 +0900

    Introduce nodejs.sh; revise git.sh

--- apt/ppa/git.sh
+++ apt/ppa/git.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo true
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt-get update
 sudo apt-get install -y git
---
+++ apt/ppa/nodejs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo true
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get -y update
+sudo apt-get -y install nodejs
