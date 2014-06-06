ecde41d994f197dc4b8c0610ebd7288cf03f9d43
katoken-0215 <katoken@katoken-0215.sakura.ne.jp>
2014-06-06 23:33:40 +0900

    Introduce devtools-haskell.sh

---
+++ apt/devtools-haskell.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo true
+sudo apt-get install -y haskell-platform
+
+cabal update
+cabal install cabal-dev
+which cabal-dev || echo 'export PATH=$PATH:$HOME/.cabal/bin' >> ~/.bashrc
