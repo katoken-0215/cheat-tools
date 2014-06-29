813742b24f546a1c1dcbb69742aaffb643de6a26
Kenji.Kato <katoken@katoken-0215.sakura.ne.jp>
2014-06-29 12:09:09 +0900

    Update devtools-haskell.sh
    Install hlint with others

--- apt/devtools-haskell.sh
+++ apt/devtools-haskell.sh
@@ -4,5 +4,5 @@ sudo true
 sudo apt-get install -y haskell-platform
 
 cabal update
-cabal install cabal-dev
+cabal install cabal-dev hlint
 which cabal-dev || echo 'export PATH=$PATH:$HOME/.cabal/bin' >> ~/.bashrc
