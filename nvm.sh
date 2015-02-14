42ea7c8e87175050d83e39e068dec1d21459d875
katoken-0215 <katoken@katoken-0215.sakura.ne.jp>
2015-02-14 23:59:38 +0900

    Remove nvm.h; Switch to nodebrew

--- nvm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.10.0/install.sh | sh
-echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
-echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm' >> ~/.bashrc
