696519011ae99d53443beda61936f3a996747f98
katoken-0215 <katoken@katoken-0215.sakura.ne.jp>
2015-02-15 00:31:52 +0900

    Introduce pyenv.sh to install python easier

---
+++ ubuntu/pyenv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -qO- https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+
+echo '# pyenv' >> ~/.bashrc
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
