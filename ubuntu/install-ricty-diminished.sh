a6fca784466b64112d78f0b0da6b8ae6830446dc
katoken-0215 <katoken@katoken-0215.sakura.ne.jp>
2016-06-15 16:38:27 +0900

    Install Ricty Diminished

---
+++ ubuntu/install-ricty-diminished.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -ue
+
+TMPDIR=$(mktemp -d)
+
+cd $TMPDIR
+wget https://github.com/yascentur/RictyDiminished/archive/4.0.1.zip
+unzip 4.0.1.zip
+
+mkdir -p ~/.fonts
+cp RictyDiminished-4.0.1/*.ttf ~/.fonts
+
+sudo fc-cache -vf
+
+cd -
+rm -rf $TMPDIR
