abd5d5b8b661f65b37aecb508f09809eca4f6835
katoken-0215 <katoken@katoken-0215.sakura.ne.jp>
2016-07-14 12:10:16 +0900

    Revise asdf installer

--- asdf.sh
+++ asdf.sh
@@ -1,9 +1,12 @@
-#!/bin/bash -ue
+#!/bin/bash
 
 sudo apt install -y automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev
 
+rm -rf ~/.asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 
+echo >> ~/.bashrc
+echo '## asdf' >> ~/.bashrc
 echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
 echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
 
@@ -12,4 +15,10 @@ echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
 asdf plugin-add go https://github.com/kennyp/asdf-golang
 asdf plugin-add node https://github.com/asdf-vm/asdf-nodejs
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby
+
+# Setup GOPATH
+echo >> ~/.bashrc
+echo '## Go' >> ~/.bashrc
+echo 'export GOPATH="$HOME/work/go"' >> ~/.bashrc
+echo 'export PATH="$GOPATH/bin:$PATH"' >> ~/.bashrc
 
