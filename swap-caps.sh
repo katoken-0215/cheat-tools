1a673c9f1133cd9d5ea35ad2d343725d88d202e4
katoken-0215 <katoken@katoken-0215.sakura.ne.jp>
2016-12-26 02:20:39 +0900

    echo needs -e to interpret backslash escape

--- swap-caps.sh
+++ swap-caps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(echo '\n# CUSTOM\nXKBOPTIONS="ctrl:nocaps"') | sudo tee -a /etc/default/keyboard > /dev/null
+(echo -e "\n# CUSTOM\nXKBOPTIONS=\"ctrl:nocaps\"") | sudo tee -a /etc/default/keyboard > /dev/null
