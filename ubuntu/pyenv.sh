05949cdc41cf28c2794c10a0ccf9cff2a74c250e
katoken-0215 <katoken@katoken-0215.sakura.ne.jp>
2017-08-20 20:15:02 +0900

    pyenv installer also installs pyenv-virtualenv

--- ubuntu/pyenv.sh
+++ ubuntu/pyenv.sh
@@ -7,3 +7,7 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 exec "$SHELL"
+
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+exec "$SHELL"
